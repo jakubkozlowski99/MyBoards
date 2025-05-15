fcc13e19efd188d3fbea314ebd112b7d52940a65
jacobofernandez420 <j.kozlowski420@interia.pl>
2025-05-15 17:38:34 +0200

    added Tag data seed

---
+++ MyBoards/Migrations/20250515153808_TagDataSeed.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    public partial class TagDataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tags",
+                columns: new[] { "Id", "Category", "Value" },
+                values: new object[,]
+                {
+                    { 1, null, "Web" },
+                    { 2, null, "UI" },
+                    { 3, null, "Desktop" },
+                    { 4, null, "API" },
+                    { 5, null, "Service" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
