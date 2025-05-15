e9ceda6679824b92dd5f6290bc0762ab349ac1df
jacobofernandez420 <j.kozlowski420@interia.pl>
2025-05-15 16:27:14 +0200

    Added seeding for WorkItemState

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -95,6 +95,11 @@ namespace MyBoards.Entities
                 .Property(s => s.Value)
                 .IsRequired()
                 .HasMaxLength(60);
+
+            modelBuilder.Entity<WorkItemState>()
+                .HasData(new WorkItemState() { Id = 1, Value = "To Do" },
+                new WorkItemState() { Id = 2, Value = "Doing" },
+                new WorkItemState() { Id = 3, Value = "Done" });
         }
     }
 }
